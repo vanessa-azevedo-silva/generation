1cf4bfad094cf69a4c63d81e135e8ac381a9c0d6
Vanessa de Azevedo Silva <vanessaazsilva@gmail.com>
2022-02-21 16:39:22 -0300

    Criando temas para o blog - Relacionamento de entidades

--- blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,12 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "postagem")
@@ -24,13 +26,17 @@ public class Postagem {
 	@NotNull
 	@Size(min = 5, max = 100)
 	private String titulo;
-	
+
 	@NotNull
 	@Size(min = 10, max = 500)
 	private String texto;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
 	public long getId() {
 		return id;
@@ -62,6 +68,14 @@ public class Postagem {
 
 	public void setData(Date data) {
 		this.data = data;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 
 }
